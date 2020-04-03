f8089cc909f27a3f9ed0e2d60ef0f62ee9345082
Jordania Pereyra <jordaniapereyra@jordanias-mbp.home>
2020-04-03 10:23:50 -0400

    styling update

--- src/Components/IndustryArticles/FeaturedBlog2.js
+++ src/Components/IndustryArticles/FeaturedBlog2.js
@@ -6,10 +6,10 @@ export default class FeauturedBlog2 extends Component {
       <>
         <div className='featuredBlogContainer2'>
 
-          <img className='mainImage' src={this.props.mainPicture2} />
-          <div className='outline'>{this.props.outline2}</div>
-          <div className='featuredBlogTitle'> {this.props.title2}</div>
-          <div className='blogSummary'> {this.props.blogSummary2} </div>
+          <img className='mainImage2' src={this.props.mainPicture2} />
+          <div className='outline2'>{this.props.outline2}</div>
+          <div className='featuredBlogTitle2'> {this.props.title2}</div>
+          <div className='blogSummary2'> {this.props.blogSummary2} </div>
 
         </div>
 
