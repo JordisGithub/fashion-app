81bae848bc98af062fbfcd10785a1310281b6db0
Jordania Pereyra <jordaniapereyra@jordanias-mbp.home>
2020-04-05 14:25:02 -0400

    published Storybook

---
+++ storybook-static/precache-manifest.bc0b4b8856b5742111581a03a73190cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97a1345210d03dca62402d10a277d0d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d156753d7450d7e99603.bundle.js"
+  },
+  {
+    "url": "runtime~main.d156753d7450d7e99603.bundle.js"
+  },
+  {
+    "revision": "5f8867d1c9b7b7f05d66",
+    "url": "static/css/main.977d45ea.chunk.css"
+  },
+  {
+    "revision": "2398f26b91efa1b1287884894dba97c3",
+    "url": "static/media/Fashion-Brunch.2398f26b.png"
+  },
+  {
+    "revision": "ed201371b25920b617f6c5e8ece34006",
+    "url": "static/media/Fast-Fashion-Cancelled.ed201371.png"
+  },
+  {
+    "revision": "6c46f743927fd94f9ebf77985fd99a74",
+    "url": "static/media/Gabriela-Hearst.6c46f743.png"
+  },
+  {
+    "revision": "cf69b4fa1800804db365829b1fc8032b",
+    "url": "static/media/Pour-Les-Femmes.cf69b4fa.png"
+  },
+  {
+    "revision": "5b51aca121be42914bceb0771958c916",
+    "url": "static/media/Reformation.5b51aca1.png"
+  },
+  {
+    "revision": "7481d095a6017f0a8411dcddb2b711b3",
+    "url": "static/media/Search.7481d095.png"
+  },
+  {
+    "revision": "fb5df8bec3a238e96ba924c2e65f242c",
+    "url": "static/media/Stella-Mccarthy.fb5df8be.png"
+  },
+  {
+    "revision": "2c50c81fb0230cffd45fbff08193355e",
+    "url": "static/media/Twitterlogo.2c50c81f.png"
+  },
+  {
+    "revision": "32b0427b8fcdf3c51e783680d65cf407",
+    "url": "static/media/instagramWhite.32b0427b.png"
+  },
+  {
+    "url": "vendors~main.d156753d7450d7e99603.bundle.js"
+  },
+  {
+    "url": "vendors~main.d156753d7450d7e99603.bundle.js.LICENSE.txt"
+  }
+]);
