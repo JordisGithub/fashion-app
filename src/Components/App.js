bd4a80ae65844cd769056c60b6ab18430a2f5601
Jordania Pereyra <Jordania.Pereyra@gmail.com>
2020-08-13 20:40:26 -0400

    app structure update

--- src/Components/App.js
+++ src/Components/App.js
@@ -37,44 +37,6 @@ import blogPicture4 from '../Images/Gabriela-Hearst.png'
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleAddComment = this.handleAddComment.bind(this);
-    this.state = {
-      comments: []
-    }
-  }
-
-  componentDidMount() {
-    /* global Ably */
-    const channel = Ably.channels.get('comments');
-
-    channel.attach();
-    channel.once('attached', () => {
-      channel.history((err, page) => {
-        /* create a new array with comments */
-        const comments = Array.from(page.items, item => item.data);
-
-        this.setState({ comments });
-
-        /* subscribe to new comments */
-        channel.subscribe((msg, err) => {
-          const commentObject = msg['data'];
-          this.handleAddComment(commentObject);
-        });
-      });
-    });
-  }
-
-
-  handleAddComment(comment) {
-    this.setState(prevState => {
-      return {
-        comments: [comment].concat(prevState.comments)
-      };
-    });
-  }
 
 
 
@@ -95,37 +57,16 @@ class App extends Component {
           title2={'FASHION SWAPS ARE THE NEW BRUCH'}
           blogSummary2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
         />
-
-
-
-        <div className='blogGrid'>
-          <BlogGrid
-            blogPicture={blogPicture}
-            title={'Reformation'}
-
-            blogPicture2={blogPicture2}
-            title2={'Stella Mccarthy'}
-
-            blogPicture3={blogPicture3}
-            title3={'Pour Les Femmes'}
-
-            blogPicture4={blogPicture4}
-            title4={'Gabriela Hearst'}
-          />
-        </div>
-
-
-
-
-        <div className="container">
-          <div className="columns">
-
-            <CommentBox handleAddComment={this.handleAddComment} />
-            <Comments comments={this.state.comments} />
-
-          </div>
-        </div>
-
+        <BlogGrid
+          blogPicture={blogPicture}
+          title={'Reformation'}
+          blogPicture2={blogPicture2}
+          title2={'Stella Mccarthy'}
+          blogPicture3={blogPicture3}
+          title3={'Pour Les Femmes'}
+          blogPicture4={blogPicture4}
+          title4={'Gabriela Hearst'}
+        />
         <Footer />
 
       </section >
